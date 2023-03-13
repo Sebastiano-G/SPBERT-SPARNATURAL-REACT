8e174cb02c590751865d1fb9b4b6650fac0ffc01
Sebastiano Giacomini <92300303+Sebastiano-G@users.noreply.github.com>
2023-03-13 16:00:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function executeIfFileExist(src, callback) {
     var xhr = new XMLHttpRequest()
     xhr.onreadystatechange = function() {
         if (this.readyState === this.DONE) {
-            callback()
+            const status = this.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+              // The request has been completed successfully
+              console.log(this.responseText);
+            }
         }
     }
     xhr.open('HEAD', src)
